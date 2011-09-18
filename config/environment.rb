afdd9ddfdf554826f0c55b0cf4767f75bb38f392
Keegan Quinn <keeganquinn@gmail.com>
2011-09-17 20:42:19 -0700

    Deleted everything of questionable value.

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-require 'dot_diskless'
-require 'transforms'
-
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-Cwnmyr::Application.initialize!
