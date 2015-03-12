8ba365c5bfdf5549319dd1fea59baa1305f06db1
Keegan Quinn <keeganquinn@gmail.com>
2015-03-12 02:34:09 -0700

    Rename migrations.

--- db/migrate/20150311000001_cwnmyr_create_tables.rb
+++ db/migrate/20150311000001_cwnmyr_create_tables.rb
@@ -1,29 +1,19 @@
+# -*- coding: utf-8 -*-
+
 #--
 # Database migration class for core cwnmyr tables
-# © 2011 Keegan Quinn
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# © 2015 Keegan Quinn
 #++
 
+
 # Database migration class for core cwnmyr tables.
-class CreateManyTables < ActiveRecord::Migration
+class CwnmyrCreateTables < ActiveRecord::Migration
   def change
     create_table(:zones) do |t|
       t.string :code, :limit => 64
       t.string :name, :limit => 128
       t.text :content
+      t.boolean :expose
       t.timestamps
     end
 
