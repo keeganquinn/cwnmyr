a2147603741265d7761e01c2ccbb0f714bb0c01b
Keegan Quinn <keeganquinn@gmail.com>
2017-05-22 19:34:07 -0700

    Spec setup and specs for Devise.

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,37 @@
+class UserPolicy
+  attr_reader :current_user, :model
+
+  def initialize(current_user, model)
+    @current_user = current_user
+    @user = model
+  end
+
+  def index?
+    @current_user.admin?
+  end
+
+  def show?
+    @current_user
+  end
+
+  def update?
+    @current_user.admin?
+  end
+
+  def destroy?
+    return false if @current_user == @user
+    @current_user.admin?
+  end
+
+  def status?
+    @current_user == @user
+  end
+
+  def queue?
+    @current_user == @user
+  end
+
+  def leave?
+    @current_user == @user
+  end
+end
---
+++ spec/policies/user_policy_spec.rb
@@ -0,0 +1,44 @@
+describe UserPolicy do
+  subject { UserPolicy }
+
+  let (:current_user) { FactoryGirl.build_stubbed :user }
+  let (:other_user) { FactoryGirl.build_stubbed :user }
+  let (:admin) { FactoryGirl.build_stubbed :user, :admin }
+
+  permissions :index? do
+    it "denies access if not an admin" do
+      expect(subject).not_to permit(current_user)
+    end
+    it "allows access for an admin" do
+      expect(subject).to permit(admin)
+    end
+  end
+
+  permissions :show? do
+    it "prevents unauthenticated access to user profiles" do
+      expect(subject).not_to permit(nil)
+    end
+    it "allows a user to see any profile" do
+      expect(subject).to permit(current_user)
+    end
+  end
+
+  permissions :update? do
+    it "prevents updates if not an admin" do
+      expect(subject).not_to permit(current_user)
+    end
+    it "allows an admin to make updates" do
+      expect(subject).to permit(admin)
+    end
+  end
+
+  permissions :destroy? do
+    it "prevents deleting yourself" do
+      expect(subject).not_to permit(current_user, current_user)
+    end
+    it "allows an admin to delete any user" do
+      expect(subject).to permit(admin, other_user)
+    end
+  end
+
+end
