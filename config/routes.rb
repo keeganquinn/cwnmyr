63a9f18fa48651bf9e396327cc90865220f872f5
Keegan Quinn <keeganquinn@gmail.com>
2017-06-13 18:55:07 -0700

    Fix crashes in configuration generator controller and templates; content still needs work.

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :nodes, :users, :zones
 
-  get 'config/index' => 'config#index', as: :config
+  get 'config' => 'config#index', as: :config
+  get 'config/dns_zone_external' => 'config#dns_zone_external'
+  get 'config/dns_zone_internal' => 'config#dns_zone_internal'
+  get 'config/smokeping_external' => 'config#smokeping_external'
+  get 'config/smokeping_internal' => 'config#smokeping_internal'
+  get 'config/nagios_external' => 'config#nagios_external'
+  get 'config/nagios_internal' => 'config#nagios_internal'
 end
