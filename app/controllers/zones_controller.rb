c3ce8fa932cdd2d87b385a33f1bbfaf4a79da553
Keegan Quinn <keeganquinn@gmail.com>
2017-06-13 19:32:17 -0700

    Clean up.

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,79 +1,26 @@
-# This controller allows the management of Zone records.
+# This controller allows viewing of Zone records.
 class ZonesController < ApplicationController
-  before_action :authenticate_user!, :except => [ :show ]
   after_action :verify_authorized
+
+  def index
+    @zones = Zone.all
+    authorize Zone
+
+    respond_to do |format|
+      format.html { redirect_to visitors_path }
+      format.json { render :json => @zones.to_json }
+      format.xml  { render :xml => @zones.to_xml }
+    end
+  end
 
   def show
     @zone = Zone.find(params[:id])
     authorize @zone
+
     respond_to do |format|
       format.html
+      format.json { render :json => @zone.to_json }
       format.xml  { render :xml => @zone.to_xml }
     end
-  end
-
-  # Display a form to allow data for a new Zone to be provided.
-  def new
-    @zone = Zone.new
-
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
-
-  # Display a form for editing a Zone record.
-  def edit
-  end
-
-  # Create a new Zone.
-  def create
-    @zone = Zone.new(zone_params)
-
-    respond_to do |format|
-      if @zone.save
-        flash[:notice] = t('zone create success')
-
-        format.html { redirect_to zone_path(@zone) }
-        format.xml  { head :created, :location => zone_path(@zone) }
-      else
-        format.html { render :action => :new }
-        format.xml  { render :xml => @zone.errors.to_xml }
-      end
-    end
-  end
-
-  # Update an existing Zone.
-  def update
-    respond_to do |format|
-      if @zone.update_attributes(params[:zone])
-        flash[:notice] = t('zone update success')
-
-        format.html { redirect_to zone_path }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => :edit }
-        format.xml  { render :xml => @zone.errors.to_xml }
-      end
-    end
-  end
-
-  # Destroy a Zone.
-  def destroy
-    @zone.destroy
-
-    flash[:notice] = t('zone destroy success')
-
-    respond_to do |format|
-      format.html { redirect_to zones_path }
-      format.xml  { head :ok }
-      format.js   { send_js "zone_#{@zone.to_param}" }
-    end
-  end
-
-  private
-
-  def zone_params
-    params.require(:zone).permit(:code, :name, :expose)
   end
 end
--- app/policies/zone_policy.rb
+++ app/policies/zone_policy.rb
@@ -6,6 +6,10 @@ class ZonePolicy
     @model = model
   end
 
+  def index?
+    true
+  end
+
   def show?
     true
   end
