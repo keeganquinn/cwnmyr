18fdc1d53eb29c88427f19a40b03e488d9c18dc8
Keegan Quinn <keeganquinn@gmail.com>
2017-06-13 19:06:40 -0700

    Remove leftover boilerplate.

--- app/controllers/interface_types_controller.rb
+++ app/controllers/interface_types_controller.rb
@@ -1,22 +1,3 @@
-#--
-# $Id: interface_types_controller.rb 514 2007-07-18 18:25:55Z keegan $
-# Copyright 2004-2007 Keegan Quinn
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#++
-
 class InterfaceTypesController < ApplicationController
   before_filter :login_required, :except => [ :index, :show ]
   before_filter :load_interface_type, :except => [ :index, :create, :new ]
--- app/controllers/user_link_controller.rb
+++ app/controllers/user_link_controller.rb
@@ -1,22 +1,3 @@
-#--
-# $Id: user_link_controller.rb 2561 2006-04-22 15:52:07Z keegan $
-# Copyright 2004-2006 Keegan Quinn
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#++
-
 class UserLinkController < ApplicationController
   before_filter :login_required
 
