b5d6617cda224271dbae5d3be94fd6acdef22134
Keegan Quinn <keeganquinn@gmail.com>
2017-07-04 13:27:08 -0700

    Prevent non-admins from accessing the admin interface.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,17 +1,14 @@
-# All Administrate controllers inherit from this `Admin::ApplicationController`,
-# making it the ideal place to put authentication logic or other
-# before_actions.
-#
-# If you want to add pagination or other controller-level concerns,
-# you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_user!
+    before_action :authenticate_admin
 
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
+    def authenticate_admin
+      redirect_to new_user_session_path, alert: t(:access_denied) unless current_user && current_user.admin?
+    end
+
+    def records_per_page
+      params[:per_page] || 50
+    end
   end
 end
