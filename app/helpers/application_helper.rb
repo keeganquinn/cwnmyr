0ecfb8ad0c19a5da299a084d41e97720a16f75a1
Keegan Quinn <keeganquinn@gmail.com>
2017-07-15 18:19:57 -0700

    Refactor and correct style.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,36 @@
+# This module includes helper methods for use in views.
 module ApplicationHelper
+  JS_INCLUDES = [
+    "//maps.googleapis.com/maps/api/js?key=#{ENV['GMAPS_API_KEY']}",
+    '//cdn.rawgit.com/mahnunchik/markerclustererplus/' \
+      'master/dist/markerclusterer.min.js',
+    '//cdn.rawgit.com/printercu/google-maps-utility-library-v3-read-only/' \
+      'master/infobox/src/infobox_packed.js'
+  ].freeze
+
+  def remote_javascript_includes
+    JS_INCLUDES.map { |js| javascript_include_tag js } .join.html_safe
+  end
+
+  MARKDOWN_OPTIONS = {
+    filter_html: true,
+    hard_wrap: true,
+    link_attributes: { rel: 'nofollow', target: '_blank' },
+    space_after_headers: true,
+    fenced_code_blocks: true
+  }.freeze
+
+  MARKDOWN_EXTENSIONS = {
+    autolink: true,
+    superscript: true,
+    disable_indented_code_blocks: true
+  }.freeze
+
   def markdown(text)
-    return if not text
+    return unless text
 
-    options = {
-      filter_html: true,
-      hard_wrap: true,
-      link_attributes: {rel: "nofollow", target: "_blank"},
-      space_after_headers: true,
-      fenced_code_blocks: true
-    }
-
-    extensions = {
-      autolink: true,
-      superscript: true,
-      disable_indented_code_blocks: true
-    }
-
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    renderer = Redcarpet::Render::HTML.new(MARKDOWN_OPTIONS)
+    markdown = Redcarpet::Markdown.new(renderer, MARKDOWN_EXTENSIONS)
 
     markdown.render(text).html_safe
   end
