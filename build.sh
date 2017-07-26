b6a508f46be7869ba09b7076353a88d08e702dd5
Keegan Quinn <keeganquinn@gmail.com>
2017-07-25 18:34:30 -0700

    More container and build tweaks.

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ set -ex
 
 rm -rf checkstyle-*.xml coverage junit.xml rspec.xml log/* public/assets tmp/*
 
-bundle exec rake db:create db:environment:set db:schema:load RAILS_ENV=test
-
-bundle exec rake spec BUILD_NUMBER="${build}" || true
-
 node_modules/.bin/jest || true
+
+node_modules/.bin/eslint \
+    app --format node_modules/eslint-formatter-checkstyle-* \
+    > checkstyle-eslint.xml || true
 
 bundle exec rubocop --require rubocop/formatter/checkstyle_formatter \
        --format RuboCop::Formatter::CheckstyleFormatter \
@@ -21,6 +21,6 @@ bundle exec rubocop --require rubocop/formatter/checkstyle_formatter \
 bundle exec haml-lint app \
        --reporter checkstyle > checkstyle-haml-lint.xml || true
 
-node_modules/.bin/eslint \
-    app --format node_modules/eslint-formatter-checkstyle-* \
-    > checkstyle-eslint.xml || true
+bundle exec rake db:create db:environment:set db:schema:load RAILS_ENV=test
+
+bundle exec rake spec BUILD_NUMBER="${build}" || true
