f68f1dfcc3b743a0134a16707ecc87d7ba26d404
Keegan Quinn <keeganquinn@gmail.com>
2017-07-25 17:12:15 -0700

    Simplify.

--- build.sh
+++ build.sh
@@ -1,36 +1,26 @@
 #!/usr/bin/env bash
 
-# Automated build script for cwnmyr. This script uses docker-compose to
-# perform a complete self-contained run of all test suites and style checks.
-# Intended for continuous integration use.
+# Automated build script for cwnmyr. Intended for continuous integration use.
 
 build=${BUILD_NUMBER:-"current"}
-compose=(docker-compose -p cwnmyr)
-exec=("${compose[@]}" exec -T web)
 
 set -ex
 
 rm -rf checkstyle-*.xml coverage junit.xml rspec.xml log/* public/assets tmp/*
 
-"${compose[@]}" down || true
-"${compose[@]}" up -d
+bundle exec rake db:create db:environment:set db:schema:load RAILS_ENV=test
 
-"${exec[@]}" rake db:create db:schema:load RAILS_ENV=test
+bundle exec rake spec BUILD_NUMBER="${build}" || true
 
-"${exec[@]}" rake spec BUILD_NUMBER="${build}" || true
+node_modules/.bin/jest || true
 
-"${exec[@]}" node_modules/.bin/jest || true
+bundle exec rubocop --require rubocop/formatter/checkstyle_formatter \
+       --format RuboCop::Formatter::CheckstyleFormatter \
+       -o checkstyle-rubocop.xml || true
 
-"${exec[@]}" rubocop \
-             --require rubocop/formatter/checkstyle_formatter \
-             --format RuboCop::Formatter::CheckstyleFormatter \
-             -o checkstyle-rubocop.xml || true
+bundle exec haml-lint app \
+       --reporter checkstyle > checkstyle-haml-lint.xml || true
 
-"${exec[@]}" haml-lint app \
-             --reporter checkstyle > checkstyle-haml-lint.xml || true
-
-"${exec[@]}" node_modules/.bin/eslint app \
-             --format node_modules/eslint-formatter-checkstyle-* \
-             > checkstyle-eslint.xml || true
-
-"${compose[@]}" down
+node_modules/.bin/eslint \
+    app --format node_modules/eslint-formatter-checkstyle-* \
+    > checkstyle-eslint.xml || true
