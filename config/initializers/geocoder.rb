a95191ddb8a9726587028501e82d072a1cc85f26
Keegan Quinn <keeganquinn@gmail.com>
2017-07-31 10:00:04 -0700

    Use default geocoder configuration in test environment.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,6 @@
-Geocoder.configure(
-  lookup: :nominatim
-)
+unless Rails.env.test?
+  Geocoder.configure(
+    http_headers: { 'User-Agent' => ENV['DOMAIN_NAME'] },
+    lookup: :nominatim
+  )
+end
