2c52246e92497b17142eb5e830a02f986c3aa345
Keegan Quinn <keeganquinn@gmail.com>
2017-08-25 21:15:00 -0700

    Add specs.

--- spec/services/create_admin_service_spec.rb
+++ spec/services/create_admin_service_spec.rb
@@ -10,4 +10,10 @@ describe CreateAdminService do
   it 'created user is an admin' do
     expect(admin).to be_admin
   end
+
+  it 'returns existing admin if there is one' do
+    existing = create :user, :admin
+    result = service.new.call
+    expect(result).to eq existing
+  end
 end
--- spec/services/create_user_service_spec.rb
+++ spec/services/create_user_service_spec.rb
@@ -10,4 +10,10 @@ describe CreateUserService do
   it 'created user is not an admin' do
     expect(user).not_to be_admin
   end
+
+  it 'returns existing user if there is one' do
+    existing = create :user
+    result = service.new.call
+    expect(result).to eq existing
+  end
 end
