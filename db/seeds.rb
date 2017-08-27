c679f88fc2a0f8395fab450b2286139e28f40529
Keegan Quinn <keeganquinn@gmail.com>
2017-08-26 19:23:08 -0700

    Add data import to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@ puts 'NORMAL USER: ' << user.email
 
 records = PopulateOptionsService.new.call
 puts 'OPTION RECORDS: ' << records.size.to_s
+
+nodes = ImportLegacyDataService.new.call
+nodes.each do |node|
+  print 'IMPORTED NODE: ', node.code, ': ', node.name, ' @ ', node.id, "\n"
+end
