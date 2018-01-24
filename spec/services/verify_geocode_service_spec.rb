f2d290f8f1b211643ccd73273ad4da8c53885f86
Keegan Quinn <keeganquinn@gmail.com>
2018-01-24 05:10:15 -0800

    Update out-of-range address.

--- spec/services/verify_geocode_service_spec.rb
+++ spec/services/verify_geocode_service_spec.rb
@@ -9,7 +9,7 @@ describe VerifyGeocodeService do
   end
 
   it 'reports nodes which were placed out of range' do
-    node = create :node, address: '34 Circle Terrace, Mason City, IA'
+    node = create :node, address: '9150 Chesapeake Drive, San Diego, CA'
     expect(service.new.call[0]).to(
       eq "#{node.id} #{node.code}: location out of range: #{node.address}"
     )
