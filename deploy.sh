601f1647efdbbb468f5772feb0fd71a90393b1ee
Keegan Quinn <keeganquinn@gmail.com>
2018-02-18 10:16:52 -0800

    Check agent before deploying, add more verbose output.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+ssh-add -l >/dev/null || ssh-add
+
 [ -d .bundle ] || mkdir -p .bundle
 [ -f .bundle/config ] || (
     echo '---'
@@ -16,8 +18,17 @@ set -e
 bundle install
 bundle exec cap staging deploy
 
-echo "Verify staging site now. Return to go live, ctrl-c to cancel."
+echo
+echo "Verify staging site now: https://cwnmyr-staging.quinn.tk/"
+echo "RET to go live, C-c to cancel."
 read -r
 
 bundle exec cap production deploy
 bundle exec cap personaltelco deploy
+
+echo
+echo "Deployed live sites:"
+echo "https://cwnmyr.quinn.tk/"
+echo "https://cwnmyr.personaltelco.net/"
+
+exit 0
