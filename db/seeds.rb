dc92f7c56aae3eaf7df3db86e60ee9c6e74c0656
Keegan Quinn <keeganquinn@gmail.com>
2019-05-21 01:32:39 -0700

    Fix string handling.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 admin = CreateAdminService.new.call
-puts 'ADMIN USER: ' << admin.email
+puts "ADMIN USER: #{admin.email}"
 
 user = CreateUserService.new.call
-puts 'NORMAL USER: ' << user.email
+puts "NORMAL USER: #{user.email}"
 
 records = PopulateOptionsService.new.call
-puts 'OPTION RECORDS: ' << records.size.to_s
+puts "OPTION RECORDS: #{records.size}"
 
-nodes = ImportLegacyDataService.new.call
-nodes.each do |node|
-  print 'IMPORTED NODE: ', node.code, ': ', node.name, ' @ ', node.id, "\n"
+importer = ImportLegacyDataService.new
+importer.call.each do |node|
+  puts "IMPORTED NODE: #{node.code}: #{node.name} @ #{node.id}"
 end
