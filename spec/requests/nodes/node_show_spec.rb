6f6ae9579e213e2a3b54d9f88f2e7383de8cc32c
Keegan Quinn <keeganquinn@gmail.com>
2019-06-01 15:34:22 -0700

    Additional specs.

---
+++ spec/requests/nodes/node_show_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+describe 'Node show action', type: :request do
+  subject { response }
+
+  context 'without an image' do
+    let(:node) { create :node }
+
+    context 'when image is requested' do
+      before { get node_path(node, format: :png) }
+
+      it { is_expected.to have_http_status(200) }
+      it { is_expected.to respond_with_content_type(:png) }
+    end
+  end
+
+  context 'with an image' do
+    let(:node) { create :node, :with_image }
+
+    context 'when image is requested' do
+      before { get node_path(node, format: :png) }
+
+      it { is_expected.to have_http_status(200) }
+      it { is_expected.to respond_with_content_type(:png) }
+    end
+  end
+end
