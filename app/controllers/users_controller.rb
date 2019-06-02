5068a447db917c61f3a994e5a88a8ea2e50371d0
Keegan Quinn <keeganquinn@gmail.com>
2019-06-01 20:19:47 -0700

    User profile management UI.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,23 @@ class UsersController < ApplicationController
 
   def show
     @user = authorize User.find(params[:id])
+    redirect_to root_path unless @user.visible?
+  end
+
+  def edit
+    authorize User.find(params[:id])
+    redirect_to edit_user_registration_path
+  end
+
+  def update
+    @user = authorize User.find(params[:id])
+    @user.assign_attributes(safe_params)
+    save_and_respond @user, :ok, :update_success
+  end
+
+  private
+
+  def safe_params
+    params.require(:user).permit(:name, :body)
   end
 end
