e5b17b94113ee3295a681ec5020ea7fd53ad7e9d
Keegan Quinn <keeganquinn@gmail.com>
2019-07-18 18:31:54 -0700

    Change minimum length validations to presence validations.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,14 +7,14 @@ class Contact < ApplicationRecord
   belongs_to :user
   belongs_to :group, optional: true
 
-  validates_length_of :code, minimum: 1
+  validates_presence_of :code
   validates_length_of :code, maximum: 64
   validates_uniqueness_of :code, case_sensitive: false
   validates_format_of :code,
                       with: /\A[-_a-zA-Z0-9]+\z/,
                       message: 'contains unacceptable characters',
                       allow_blank: true
-  validates_length_of :name, minimum: 1
+  validates_presence_of :name
   validates_format_of :email,
                       with: /\A([\w\-\.\#\$%&!?*\'=(){}|~_]+)
                             @([0-9a-zA-Z\-\.\#\$%&!?*\'=(){}|~]+)+\z/x,
