9e38c1c6c16dd49c9da364f539a1727605c31f3b
Keegan Quinn <keeganquinn@gmail.com>
2019-07-21 18:43:49 -0700

    Update specs.

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -8,14 +8,14 @@ module Features
       fill_in 'Email', with: email
       fill_in 'Password', with: password
       fill_in 'Password Confirmation', with: confirmation
-      click_button 'Sign up'
+      click_button 'Sign Up'
     end
 
     def signin(email, password)
       visit new_user_session_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
-      click_button 'Log in'
+      click_button 'Sign In'
     end
   end
 end
