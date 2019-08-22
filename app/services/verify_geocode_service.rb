839cff2eac2fc5889c724c69b53f5339c5b57518
Keegan Quinn <keeganquinn@gmail.com>
2019-08-22 16:39:14 -0700

    Verify geocoding based on zone center location instead of hardcoded value.

--- app/services/verify_geocode_service.rb
+++ app/services/verify_geocode_service.rb
@@ -2,23 +2,19 @@
 
 # Service to verify geocoding results.
 class VerifyGeocodeService
-  # Should stick this in the zone eventually.
-  EXPECTED_LATITUDE = 45.51894475
-  EXPECTED_LONGITUDE = -122.679545541875
-
   def call
     Node.where.not(address: nil).map do |node|
       next if node.address.blank?
 
-      check_range(node) || next
+      check_range(node, node.zone.latitude, node.zone.longitude) || next
     end.compact
   end
 
-  def check_range(node)
+  def check_range(node, expected_latitude, expected_longitude)
     return unless node.latitude && node.longitude
 
-    lat_diff = (EXPECTED_LATITUDE - node.latitude).abs
-    long_diff = (EXPECTED_LONGITUDE - node.longitude).abs
+    lat_diff = (expected_latitude - node.latitude).abs
+    long_diff = (expected_longitude - node.longitude).abs
     return unless lat_diff >= 0.5 || long_diff >= 0.5
 
     "#{node.id} #{node.code}: location out of range: #{node.address}"
--- spec/services/verify_geocode_service_spec.rb
+++ spec/services/verify_geocode_service_spec.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+DISTANT_ADDRESS = '9150 Chesapeake Drive, San Diego, CA'
+LOCAL_ADDRESS = '727 SE Grand Ave, Portland, OR 97214'
+
 describe VerifyGeocodeService do
   subject(:service) { described_class }
 
+  let(:zone) { create :zone, address: LOCAL_ADDRESS }
+
   it 'reports nodes which were placed out of range' do
-    node = create :node, address: '9150 Chesapeake Drive, San Diego, CA'
+    node = create :node, zone: zone, address: DISTANT_ADDRESS
     expect(service.new.call[0]).to(
       eq "#{node.id} #{node.code}: location out of range: #{node.address}"
     )
