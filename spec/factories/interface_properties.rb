d5bced2cc4f802efec34b2dfc2d73172690ef562
Keegan Quinn <keeganquinn@gmail.com>
2019-08-26 23:50:04 -0700

    Remove old factory.

--- spec/factories/interface_properties.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :interface_property do
-    interface
-    sequence(:key) { |n| "key#{n}" }
-    sequence(:value) { |n| "value#{n}" }
-  end
-end
