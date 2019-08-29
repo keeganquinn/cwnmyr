dbc2eccbd18428e0854ad2ae6adc77efbadf46ae
Keegan Quinn <keeganquinn@gmail.com>
2019-08-28 20:02:28 -0700

    Add spec for browse page.

---
+++ spec/features/visitors/browse_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+describe 'Browse page', type: :feature do
+  let(:zone) { create :zone, default: true }
+  let!(:node) { create :node, zone: zone }
+
+  before { visit browse_path }
+
+  it { expect(page).to have_content node.name }
+end
