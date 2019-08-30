b80ef65d66df16af73341be8f732f5d9862f40b9
Keegan Quinn <keeganquinn@gmail.com>
2019-08-29 22:23:44 -0700

    Remove unused classes.

--- app/mailers/application_mailer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# This class is parent to all mailers in the application.
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-end
