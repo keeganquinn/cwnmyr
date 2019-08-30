ad3569f50f10ced0670e1eae88b76ef1d9fa3c22
Keegan Quinn <keeganquinn@gmail.com>
2019-08-30 15:01:40 -0700

    Style corrections.

--- spec/mailers/users/mailer_spec.rb
+++ spec/mailers/users/mailer_spec.rb
@@ -4,31 +4,31 @@ describe Users::Mailer, type: :mailer do
   let(:user) { build_stubbed :user }
 
   describe 'confirmation_instructions' do
-    let(:mail) { Users::Mailer.confirmation_instructions(user, 'abc') }
+    let(:mail) { described_class.confirmation_instructions(user, 'abc') }
 
     it { expect(mail.subject).to match(/(.*)?: Confirmation Instructions/) }
   end
 
   describe 'reset_password_instructions' do
-    let(:mail) { Users::Mailer.reset_password_instructions(user, 'abc') }
+    let(:mail) { described_class.reset_password_instructions(user, 'abc') }
 
     it { expect(mail.subject).to match(/(.*)?: Reset Password Instructions/) }
   end
 
   describe 'unlock_instructions' do
-    let(:mail) { Users::Mailer.unlock_instructions(user, 'abc') }
+    let(:mail) { described_class.unlock_instructions(user, 'abc') }
 
     it { expect(mail.subject).to match(/(.*)?: Unlock Instructions/) }
   end
 
   describe 'email_changed' do
-    let(:mail) { Users::Mailer.email_changed(user) }
+    let(:mail) { described_class.email_changed(user) }
 
     it { expect(mail.subject).to match(/(.*)?: Email Changed/) }
   end
 
   describe 'password_change' do
-    let(:mail) { Users::Mailer.password_change(user) }
+    let(:mail) { described_class.password_change(user) }
 
     it { expect(mail.subject).to match(/(.*)?: Password Change/) }
   end
