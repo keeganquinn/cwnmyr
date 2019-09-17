a1d21077e4c80844baf031e060cb6de582df56d4
Keegan Quinn <keeganquinn@gmail.com>
2019-09-16 22:57:48 -0700

    Add spec for admin user update.

--- spec/features/admin/users_spec.rb
+++ spec/features/admin/users_spec.rb
@@ -30,4 +30,17 @@ describe 'Users admin interface', type: :feature do
     it { expect(page).to have_current_path(admin_users_path) }
     it { expect(page).to have_content 'Users' }
   end
+
+  describe 'admin user updates a user' do
+    let(:user) { create :user }
+    let(:admin) { create :user, :admin }
+
+    before do
+      login_as admin
+      visit edit_admin_user_path(user)
+      click_on 'Update User'
+    end
+
+    it { expect(page).to have_current_path admin_user_path(user) }
+  end
 end
