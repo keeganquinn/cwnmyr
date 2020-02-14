4d4fbe5821be8bb694a1fa6e0b65e9bb9cecad78
Keegan Quinn <keegan@quinn.tk>
2020-02-14 10:03:20 -0800

    Polish and improve daily cron job script.

--- daily.sh
+++ daily.sh
@@ -3,8 +3,13 @@
 # Run cwnmyr seed task.
 
 export PATH="$HOME/.rbenv/bin:$PATH"
+export RAILS_ENV=${RAILS_ENV:-"production"}
+export RUBYOPT=-W0
 eval "$(rbenv init -)"
 
 set -e
 
-(cd /srv/rails/cwnmyr/current; RAILS_ENV=production bundle exec rake db:seed)
+cwnmyr="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${cwnmyr}"
+
+bundle exec rake db:seed
