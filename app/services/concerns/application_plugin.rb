d3b47567a27e283024a4904891cc2b0adbfdd16d
Keegan Quinn <keegan@quinn.tk>
2020-02-15 23:20:13 -0800

    Harden.

--- app/services/concerns/application_plugin.rb
+++ app/services/concerns/application_plugin.rb
@@ -6,17 +6,22 @@ class ApplicationPlugin
 
   # Determine the hostmask of the current command.
   def hostmask
-    "#{msg.user.user}@#{msg.user.host}"
+    "#{msg.user.user}@#{msg.user.host}" if msg&.user
   end
 
   # Locate the current authorized User.
   def current_user
-    User.find_by hostmask: hostmask
+    User.find_by hostmask: hostmask if hostmask
   end
 
   # Handle a matched request.
   def execute(message)
     @msg = message
     respond
+  end
+
+  # Handle a listener request.
+  def listen(message)
+    execute message
   end
 end
